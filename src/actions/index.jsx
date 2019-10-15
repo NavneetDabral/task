46f3370bc4c07c571c1b5b00bea2b715c6f94ccb
Navneet Dabral <dabralnavneet@gmail.com>
2019-10-15 10:30:20 +0530

    Add files via upload

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,1 +1,8 @@
-
+export const ADD_CSV_DATA ="add_csv_data";
+export const ADD_COLUMNS="add_columns";
+export const ADD_USER_INFO="add_user_info";
+export const ADD_LOCATION_INFO="add_location_info";
+export const ADD_MONTHLY_DATA="add_monthly_data";
+export const LOADER_FLAG="loader_flag";
+export const REMOVE_MONTHLY_DATA="remove_monthly_data";
+export const REMOVE_TEST="remove_test";
